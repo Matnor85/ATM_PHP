c3be864076bcd9ecb17cc394a65cf4e2e13076a7
Mathias <Mathias.norrbacken@gmail.com>
2026-05-27 23:43:57 +0200

    fixat bugg

--- templates/around-the-corner-open-door.php
+++ templates/around-the-corner-open-door.php
@@ -10,20 +10,19 @@
     <body>
         <main>
             <div class="image-container">
-                
-                <a href="index.php?page=around-the-corner-closed-door" class="back-button" title="Stäng dörren">&#8592;</a>
-                
-                <img src="assets/img/around-corner.png" class="atm-image" id="atm-bg" alt="Bankomat med öppen dörr"/>
+                <img
+                    src="assets/img/around-corner.png"
+                    class="atm-image"
+                    id="atm-bg"
+                    alt="Bankomat med öppen dörr"
+                />
 
                 <canvas id="matrix" class="matrix-canvas"></canvas>
+
                 <div id="server-overlay" class="atm-hotspot"></div>
 
-                <a href="index.php?page=admin_login" class="atm-hotspot" style="left:13.5%; top:43.6%; width:3.9%; height:6%;" title="Admin-dator"></a>
-
-                <form method="POST" action="index.php" style="position: absolute; left: 8.8%; top: 30%; width: 4%; height: 31.5%; z-index: 50;">
-                    <input type="hidden" name="action" value="install_database">
-                    <button type="submit" style="width: 100%; height: 100%; background: transparent; border: none; cursor: pointer; outline: none;" title="Återställ och seeda databas"></button>
-                </form>
+                <a href="index.php?page=around-the-corner-closed-door" class="atm-hotspot" style="left:5.3%;top:42.2%;width:2%;height:6%;" title="Stäng dörren"></a>
+                <a href="index.php?page=admin_login"                                  class="atm-hotspot" style="left:13.5%;top:43.6%;width:3.9%;height:6%;" title="Admin-dator"></a>
 
                 <script src="assets/js/matrix.js"></script>
             </div>
